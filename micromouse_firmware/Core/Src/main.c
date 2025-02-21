ae21b822f14425b5171c3f976ac95b0b35e1427b
ShimaMichael <99474784+ShimaMichael@users.noreply.github.com>
2025-02-21 20:08:41 +0000

    added peripherals (#6)
    i dont know what the code does at all

---
+++ micromouse_firmware/Core/Src/main.c
@@ -0,0 +1,14 @@
+
+int main()
+{
+    //initialize system
+
+    while(1)
+    {
+        //read sensors
+        //update position
+        //update map
+        //decide next move
+        //move
+    }
+}
